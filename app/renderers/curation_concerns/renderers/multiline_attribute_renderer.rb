85e08c5c90cf800ceeb95a7f9b5b9c9854d0130e
Stefano Cossu <scossu@artic.edu>
2016-08-15 15:43:54 -0500

    Style fixes.

--- app/renderers/curation_concerns/renderers/multiline_attribute_renderer.rb
+++ app/renderers/curation_concerns/renderers/multiline_attribute_renderer.rb
@@ -2,14 +2,14 @@
 module CurationConcerns
   module Renderers
     class MultilineAttributeRenderer < AttributeRenderer
-        def attribute_value_to_html(value)
-          ml_value = value.gsub(/(?:\n\r?|\r\n?)/, "<br/>").html_safe
-          if microdata_value_attributes(field).present?
-            "<span#{html_attributes(microdata_value_attributes(field))}>#{li_value(ml_value)}</span>"
-          else
-            li_value(ml_value)
-          end
+      def attribute_value_to_html(value)
+        ml_value = value.gsub(/(?:\n\r?|\r\n?)/, "<br/>").html_safe
+        if microdata_value_attributes(field).present?
+          "<span#{html_attributes(microdata_value_attributes(field))}>#{li_value(ml_value)}</span>"
+        else
+          li_value(ml_value)
         end
+      end
     end
   end
 end
