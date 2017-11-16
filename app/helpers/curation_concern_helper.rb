de22cc71e0142fb96cd30238ba4907c24129a4a7
Kevin Musiorski <kmusio@artic.edu>
2017-11-16 16:11:24 -0600

    wrote two tests. actually had a bug in my code b/c it couldnt' access action_name inside the helper method, so I ended up having to pass that arg. yikes. Gooooo testing!

--- app/helpers/curation_concern_helper.rb
+++ app/helpers/curation_concern_helper.rb
@@ -3,7 +3,7 @@ module CurationConcernHelper
   # https://cits.artic.edu/redmine/issues/2183
   # we needed different titles when uploading single asset vs multiple assets
   # if it's a GenericWork, it's a single asset, if it's not, it's a BatchUploadItem
-  def custom_h1(curation_concern)
+  def custom_h1(curation_concern, action_name)
     if curation_concern.is_a?(GenericWork)
       "Create New Asset"
     else
---
+++ spec/helpers/curation_concern_helper_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+describe CurationConcernHelper do
+  describe "#custom_h1" do
+    let(:gw) {GenericWork.new}
+    let(:bui) {BatchUploadItem.new}
+    let(:action_name) {"new"}
+
+    it "returns custom string for single new GenericWork asset" do
+      expect(helper.custom_h1(gw, action_name)).to eq("Create New Asset")
+    end
+    it "returns something different for a new non-GenericWork" do
+      expect(helper.custom_h1(bui, action_name)).not_to eq("Create New Asset")
+    end
+  end
+end
