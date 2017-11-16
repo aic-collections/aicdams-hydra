6003cc9b74b1b974e6bb2d2f25095eb5841dc9f4
Kevin Musiorski <kmusio@artic.edu>
2017-11-16 17:17:59 -0600

    rubocop spaces inside brackets...

--- spec/helpers/curation_concern_helper_spec.rb
+++ spec/helpers/curation_concern_helper_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 
 describe CurationConcernHelper do
   describe "#custom_h1" do
-    let(:gw) {GenericWork.new}
-    let(:bui) {BatchUploadItem.new}
-    let(:action_name) {"new"}
+    let(:gw) { GenericWork.new }
+    let(:bui) { BatchUploadItem.new }
+    let(:action_name) { "new" }
 
     it "returns custom string for single new GenericWork asset" do
       expect(helper.custom_h1(gw, action_name)).to eq("Create New Asset")
