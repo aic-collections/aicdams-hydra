ddb44475664ac4b6b5d499f80a98e2eab5a94204
Kevin Musiorski <rudyonrails@gmail.com>
2017-11-17 11:00:13 -0600

    Update curation_concern_helper_spec.rb

--- spec/helpers/curation_concern_helper_spec.rb
+++ spec/helpers/curation_concern_helper_spec.rb
@@ -5,13 +5,12 @@ describe CurationConcernHelper do
   describe "#custom_h1" do
     let(:gw) { GenericWork.new }
     let(:bui) { BatchUploadItem.new }
-    let(:action_name) { "new" }
 
     it "returns custom string for single new GenericWork asset" do
-      expect(helper.custom_h1(gw, action_name)).to eq("Create New Asset")
+      expect(helper.custom_h1(gw)).to eq("Create New Asset")
     end
     it "returns something different for a new non-GenericWork" do
-      expect(helper.custom_h1(bui, action_name)).not_to eq("Create New Asset")
+      expect(helper.custom_h1(bui)).not_to eq("Create New Asset")
     end
   end
 end
