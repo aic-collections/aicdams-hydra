95899611719789c8abebd2f3be79497bfa6347d6
Kevin Musiorski <kmusio@artic.edu>
2018-08-01 02:48:33 -0500

    riiif cache duration for file_store

--- config/initializers/riiif.rb
+++ config/initializers/riiif.rb
@@ -17,4 +17,4 @@ end
 Riiif::Image.authorization_service = IIIFAuthorizationService
 
 Riiif.not_found_image = 'app/assets/images/us_404.svg'
-Riiif::Engine.config.cache_duration_in_days = 365
+Riiif::Engine.config.cache_duration_in_days = 0
