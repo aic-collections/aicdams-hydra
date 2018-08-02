19ad0217b8827d1ee11e375275bd936e0fab7b99
Kevin Musiorski <kmusio@artic.edu>
2018-08-01 19:43:12 -0500

    decode id in params, and pass through to Riiif render so that cache_key is unique and tiles don't get mixed. also reduces long file id down to file_set in order to find derivative and file_set in solr

--- app/prepends/prepended_controllers/with_stripped_down_file_set_id.rb
+++ app/prepends/prepended_controllers/with_stripped_down_file_set_id.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 module PrependedControllers::WithStrippedDownFileSetId
   def image_id
-    params[:id].partition("%").first
+    URI.unescape(params[:id])
   end
 end
