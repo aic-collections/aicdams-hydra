b3837653b2fe51a3370c6213efe3dd26a0f3cb93
Kevin Musiorski <kmusio@artic.edu>
2018-08-08 15:39:25 -0500

    3055_toast_sufia_uploaded_file

--- app/jobs/lakeshore/create_all_derivatives.rb
+++ app/jobs/lakeshore/create_all_derivatives.rb
@@ -3,6 +3,12 @@
 class Lakeshore::CreateAllDerivatives < ActiveJob::Base
   queue_as :api
 
+  after_perform do |job|
+    fs_uri = job.arguments.first.uri.to_s
+    sufia_uploaded_file = Sufia::UploadedFile.find_by_file_set_uri(fs_uri)
+    sufia_uploaded_file.destroy if sufia_uploaded_file
+  end
+
   # @param [FileSet] file_set
   # @param [String] file_id identifier for a Hydra::PCDM::File
   # @param [String, NilClass] filepath the cached file within the CurationConcerns.config.working_path
