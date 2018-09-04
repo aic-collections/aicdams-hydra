6ee8f0af2bc8ebde994c82eb6033c8a2d5278912
Kevin Musiorski <kmusio@artic.edu>
2018-09-04 18:04:21 -0500

    small tweaks

--- app/controllers/lakeshore/thumbnails_controller.rb
+++ app/controllers/lakeshore/thumbnails_controller.rb
@@ -2,8 +2,13 @@
 module Lakeshore
   class ThumbnailsController < PubAPIController
     include CurationConcerns::DownloadBehavior
+    include DownloadBehavior
 
-    before_action :set_file_set_id_from_asset_uuid
+    before_action :set_file_set_id_from_asset_uuid, :set_file
+
+    def set_file
+      params[:file] = "thumbnail"
+    end
 
     def set_file_set_id_from_asset_uuid
       asset = GenericWork.find(params[:asset_uuid])
