c275ce8ab3a29e4919619486a2663bb9b031d171
Kevin Musiorski <kmusio@artic.edu>
2018-10-12 15:46:49 -0500

    takes out method already in parent class, HiddenMultiSelectInput
    adds new input-class data attr so we can if-else the html in the javascript

--- app/inputs/citi_resources_multi_select_input.rb
+++ app/inputs/citi_resources_multi_select_input.rb
@@ -15,6 +15,7 @@ class CitiResourcesMultiSelectInput < HiddenMultiSelectInput
         <input type="hidden" class="autocomplete bigdrop #{attribute_name}" data-placeholder="#{placeholder}" data-endpoint="#{endpoint}" data-minchars="#{minchars}"
            id="#{input_html_options[:id]}">
         <a href="#" class="am-add btn btn-success"
+           data-input-class="citi_resources_multi_select"
            data-attribute="#{attribute_name}"
            data-model="#{object.model.class.to_s.downcase}"
            data-name="#{input_html_options[:name]}">+ Add</a>
@@ -34,14 +35,6 @@ class CitiResourcesMultiSelectInput < HiddenMultiSelectInput
       HTML
     end
 
-    def inner_wrapper
-      <<-HTML
-          <tr class="field-wrapper">
-            #{yield}
-          </tr>
-        HTML
-    end
-
     # TODO: Form object should create solr doc
     def inner_field_wrapper(_value, index)
       <<-HTML
@@ -57,45 +50,5 @@ class CitiResourcesMultiSelectInput < HiddenMultiSelectInput
         </td>
         <td><a href="#" class="btn btn-danger am-delete">- Remove</a></td>
       HTML
-    end
-
-  private
-
-    def get_data_attribute(html_input_options: options, data_attribute:)
-      if html_input_options && html_input_options[:data] && html_input_options[:data][:"#{data_attribute}"]
-
-        html_input_options[:data][:"#{data_attribute}"]
-      end
-    end
-
-    def build_field_options(value)
-      { value: value, name: input_html_options[:name] }
-    end
-
-    def build_field(value, index)
-      options = build_field_options(value_for_input(value))
-      inner_field_wrapper(value, index) do
-        @builder.hidden_field(attribute_name, options)
-      end
-    end
-
-    def collection
-      @collection = object.send(attribute_name)
-    end
-
-    def resources
-      @resources ||= object.send(options.fetch(:property, attribute_name))
-    end
-
-    # @param [SolrDocument, GenericWork] resource
-    # @todo Further optimizations would make this method take only a SolrDocument
-    def render_thumbnail(resource)
-      solr_document = resource.is_a?(SolrDocument) ? resource : SolrDocument.new(resource.to_solr)
-      template.render_thumbnail_tag(solr_document)
-    end
-
-    def value_for_input(value)
-      return value if value.is_a?(String)
-      value.id
     end
 end
